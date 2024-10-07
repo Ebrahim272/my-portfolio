5b3f0cee848892edd6df18b5b8bf5b36930bfba8
Your Name <you@example.com>
2024-10-07 23:27:05 +0430

    feat contact us with email

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -9,7 +9,6 @@ type ServicesProps={
   id:string,
   title:string,
   description:string,
-  href:string,
 }
 type ServicesData={
   services:ServicesProps[]
@@ -30,9 +29,9 @@ export default function Services({services}:ServicesData) {
             <div key={index} className='flex-1 flex flex-col justify-center gap-6 group'>
               <div className='w-full flex justify-between items-center'>
               <div className='text-5xl font-semibold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500 '>{service.id}</div>
-              <Link href={service.href} className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
+              <div className='w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45'>
               <BsArrowDownRight className='text-primary text-3xl'/>
-                </Link>
+                </div>
               </div>
               <h1 className='text-2xl font-bold text-white group-hover:text-accent transition-all duration-500'>{service.title}</h1>
               <p className='text-white/60'>{service.description}</p>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,26 +1,22 @@
 export const services = [
   {
     id: "01",
-    title: "Full Stack Web Development",
-    description: "I specialize in building dynamic, responsive websites and web applications. Using technologies like React, Next.js, Node.js, and MongoDB, I can create seamless, fast, and scalable solutions tailored to your needs.",
-    href: "/portfolio/full-stack-web-development"
-  },
+    title: "Web Developer",
+    description: "As a web developer, I focus on creating responsive, high-performance websites and web applications. With a strong expertise in front-end technologies such as React, Next.js, and Tailwind CSS, I build user-friendly, visually appealing interfaces.",
+  },    
   {
     id: "02",
     title: "UI/UX Design",
     description: "I focus on designing intuitive, user-friendly interfaces. From wireframes to high-fidelity prototypes, I ensure that every product I design provides a seamless and enjoyable experience for users.",
-    href: "/portfolio/ui-ux-design"
   },
   {
     id: "03",
     title: "Mobile App Development",
     description: "I develop cross-platform mobile apps using React Native, enabling businesses to reach users on both iOS and Android devices with a single codebase, without compromising on performance or user experience.",
-    href: "/portfolio/mobile-app-development"
   },
   {
     id: "04",
     title: "SEO & Performance Optimization",
     description: "I can help your website rank higher in search engines and optimize your site’s performance to ensure faster load times, providing both a better user experience and improved visibility.",
-    href: "/portfolio/seo-performance-optimization"
   }
 ];
